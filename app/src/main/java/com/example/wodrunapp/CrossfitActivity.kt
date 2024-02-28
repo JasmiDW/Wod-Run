c3482a2dda766d81098cdc8d60a4c5ec133a92f5
Nadia <nadia.rousseau@hotmail.fr>
2024-02-28 20:47:43 +0100

    :lipstick: Update bottom nav

--- app/src/main/java/com/example/wodrunapp/CrossfitActivity.kt
+++ app/src/main/java/com/example/wodrunapp/CrossfitActivity.kt
@@ -130,7 +130,6 @@ class SpacesItemDecoration(private val space: Int) : RecyclerView.ItemDecoration
         outRect.right = space
         outRect.bottom = space
 
-        // Add top margin only for the first item to avoid double space between items
         if (parent.getChildLayoutPosition(view) == 0) {
             outRect.top = space
         }
