97485b4c9c8adcf64e3bf74fb15ca30de3f78cdd
Nadia <nadia.rousseau@hotmail.fr>
2024-02-28 21:48:44 +0100

    :white_check_mark: Tests CrossfitActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.4.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -65,6 +66,7 @@ dependencies {
 
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.transition:transition:1.4.1")
+    implementation("org.osmdroid:osmdroid-android:6.1.18")
 
 
 
--- app/src/androidTest/java/com/example/wodrunapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/wodrunapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,31 @@
 package com.example.wodrunapp
 
+
+import androidx.test.espresso.Espresso.onView
+
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+
+import java.util.regex.Pattern.matches
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +34,35 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule = ActivityScenarioRule(CrossfitActivity::class.java)
+
+    @Before
+    fun setUp() {
+        Intents.init()
+    }
+    @Test
+    fun testNavigationToRunActivity() {
+        onView(withId(R.id.nav_run)).perform(click())
+        intended(hasComponent(RunActivity::class.java.name))
+    }
+
+    @Test
+    fun testNavigationToAddRecord() {
+        onView(withId(R.id.button)).perform(click())
+        intended(hasComponent(RecordActivity::class.java.name))
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.wodrunapp", appContext.packageName)
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
     }
 }
