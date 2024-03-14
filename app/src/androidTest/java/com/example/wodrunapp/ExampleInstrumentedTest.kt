df0fcdab89455a45119bcdf86f3a52736b344179
Nadia <nadia.rousseau@hotmail.fr>
2024-03-14 16:17:39 +0100

    :white_check_mark: Tests Partie Wod

--- app/src/androidTest/java/com/example/wodrunapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/wodrunapp/ExampleInstrumentedTest.kt
@@ -4,14 +4,10 @@ package com.example.wodrunapp
 import androidx.test.espresso.Espresso.onView
 
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -25,7 +21,6 @@ import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 
-import java.util.regex.Pattern.matches
 
 /**
  * Instrumented test, which will execute on an Android device.
